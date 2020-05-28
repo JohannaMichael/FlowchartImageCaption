fd4d1d62cca925f6e521b4a0237a59fa541f520b
JohannaMichael <jm@cliomail.com>
2020-05-28 12:01:11 +0200

    Selective Search can now be done on a list of images. The selected objects are then saved again as images with specific names for identification

--- FlowchartSegmentationTechnique/SelectiveSearch.py
+++ FlowchartSegmentationTechnique/SelectiveSearch.py
@@ -1,5 +1,7 @@
+import glob
 import cv2
 import numpy as np
+import re
 
 
 def selective_search(img, method):
@@ -44,10 +46,10 @@ def selective_search(img, method):
         area = w * h
         # Wenn die Fläche über 90 000 oder unter 15 000 Pixel groß ist oder die Höhe oder Breite unter 150/100 Pixel,
         # wird das Rechteck entfernt.
-        if area > 90000 or area < 18000:
+        if area > 90000 or area < 18000 or w < 50 or h < 80 or w > 300 or h > 300:
             rectsToDelete.append(i)
 
-    print(len(rectsToDelete), 'gefiltert.', len(rects) - len(rectsToDelete), 'Regionen werden übergeben')
+    print(len(rectsToDelete), ' filtered. ', len(rects) - len(rectsToDelete), ' regions are passed on.')
 
     # Regionen werden gelöscht
     for j in reversed(rectsToDelete):
@@ -115,31 +117,92 @@ def showPic(imgName, selectedObjects):
     return imOut
 
 
-folder_name = 'FlowchartData/Images/'
-img_name = '00000008'
+# Gibt True an, wenn sich zwei Bounding-Boxes zu viel überschneiden (da wahrscheinlich selbes Motiv)
+def intersection(boxA, boxB):
+    # Punkte oben rechts und unten links werden errechnet
+    # boxA = [rectA[0], rectA[1], rectA[0] + rectA[2], rectA[1]+rectA[3]]
+    # boxB = [rectB[0], rectB[1], rectB[0] + rectB[2], rectB[1] + rectB[3]]
 
-data_name = folder_name + img_name + '.jpg'
+    # Speichert die Koordinaten der Überschneidungsfläche (die Koordinaten des Punktes links oben und des Punktes rechts unten)
+    interXmin = max(boxA[0], boxB[0])
+    interYmin = max(boxA[1], boxB[1])
+    interXmax = min(boxA[2], boxB[2])
+    interYmax = min(boxA[3], boxB[3])
 
-# Bild wird eingelesen
-img = cv2.imread(data_name, cv2.IMREAD_COLOR)
-if img is None:
-    print('img: ' + data_name + ' fail!')
-    exit(0)
-else:
-    print('img: ' + data_name + ' read!')
+    # Berechnet die Überschneidungsfläche
+    interArea = max(0, interXmax - interXmin) * max(0, interYmax - interYmin)
 
-# Selective Search wird durchgeführt.
-scaledBoundingBoxes, selectedObjects = selective_search(img, 'f')
-if scaledBoundingBoxes and selectedObjects is None:
-    print('Selective Search, fail!')
-    exit(0)
-else:
-    print('Selective Search, done!')
+    # Die Flächen der Boxen werden berechnet
+    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
 
-finalPics = showPic(img_name, selectedObjects)
+    # print('BoxAArea: ' + str(boxAArea) + ' / BoxBArea: ' + str(boxAArea))
 
-if len(finalPics) == 0 or finalPics is None:
-    print('Showing, fail!')
-    exit(0)
-else:
-    print('Showing, done!')
+    # Berechnet die größe des Anteils der Überschneidungsfläche an den beiden Bounding Boxes
+    ioboxA = interArea / boxAArea
+    ioboxB = interArea / boxBArea
+
+    # Gibt den kleineren Anteil zurück
+    minInter = min(ioboxA, ioboxB)
+
+    # Gibt True an, wenn eines der beiden Bounding Boxes zu über 80% in der anderen liegt,
+    # der Wert bei keiner der beiden jedoch unter 50% ist
+    return (ioboxA > 0.8 or ioboxB > 0.8) and minInter > 0.7
+
+
+def deleteIntersectedObjects(scaledBoundingBoxes, selectedObjects, imgNumber):
+    i = 0
+    numberOfIntersections = 0
+    arraylen = len(scaledBoundingBoxes)
+    while i < arraylen:
+        j = i + 1
+        while j < arraylen:
+            if intersection(scaledBoundingBoxes[i], scaledBoundingBoxes[j]):
+                numberOfIntersections += 1
+                scaledBoundingBoxes.pop(j)
+                selectedObjects.pop(j)
+                j -= 1
+                arraylen -= 1
+                break
+            j += 1
+        i += 1
+    print('Number of objects selected that did not intersect each other: ' + str(len(selectedObjects)))
+    saveSelectedObject(selectedObjects, imgNumber)
+    return selectedObjects
+
+
+def saveSelectedObject(selectedObjects, imgNumber):
+    selectedObjectNum = 0
+    for image in selectedObjects:
+        selectedObjectNum += 1
+        cv2.imwrite('FlowchartData/SelectedObjects/' + str(imgNumber) + '_' + str(selectedObjectNum) + '.jpg',
+                    image)
+
+
+# Below path contains all the images
+flowcharts_folder_name = './FlowchartData/Images/'
+# Create a list of all image names in the directory
+flowchart_path_list = glob.glob(flowcharts_folder_name + '*.jpg')
+
+for path in flowchart_path_list:
+    path_number = int(re.search(r'\d+', path).group())
+    if path_number > 303:
+        # print(path)
+        # print(int(re.search(r'\d+', path).group()))
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        if img is None:
+            print('img: ' + path + ' fail!')
+            exit(0)
+        else:
+            print('img: ' + path + ' read!')
+
+        scaledBoundingBoxes, selectedObjects = selective_search(img, 'f')
+        if scaledBoundingBoxes and selectedObjects is None:
+            print('Selective Search, fail!')
+            exit(0)
+        else:
+            print('Selective Search, done!')
+
+        img_number = int(re.search(r'\d+', path).group())
+        reducedSelectedObjects = deleteIntersectedObjects(scaledBoundingBoxes, selectedObjects, img_number)
+        # finalPics = showPic(img_name, reducedSelectedObjects)
